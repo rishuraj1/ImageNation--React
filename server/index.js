d28602fd5eebbecf49c8a13b52665084fec98a68
Rishu <rrishu561@gmail.com>
2023-06-29 18:29:28 +0530

    fix: cors err

--- server/index.js
+++ server/index.js
@@ -11,7 +11,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    }
+));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/images', imageRoutes);
