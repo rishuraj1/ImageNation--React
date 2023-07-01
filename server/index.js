f7ad7f50a6d6e6eff5cc9ca28bfa522b4aa17e63
Rishu <rrishu561@gmail.com>
2023-07-01 22:17:31 +0530

    update: more images

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: "*",
+        origin: ['http://localhost:5173', 'http://localhost:8080'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
