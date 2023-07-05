17fc0f363a4870d24cb64fe05deec60fbecc1629
Rishu <rrishu561@gmail.com>
2023-07-05 13:49:23 +0530

    resolve: cors err in dalleRoutes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
+import cors from 'cors'
 
 dotenv.config();
 
 const router = express.Router();
+
+router.use(cors(
+    {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: "Content-Type, Authorization, Origin, X-Requested-With, Accept",
+    }
+));
 
 router.route('/').post(async (req, res) => {
     try {
@@ -16,12 +25,18 @@ router.route('/').post(async (req, res) => {
 
         const openai = new OpenAIApi(configuration);
 
+        const headers = {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        }
+
         const aiRes = await openai.createImage({
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
-        });
+        }, {headers});
 
         const image = aiRes.data.data[0].b64_json;
 
